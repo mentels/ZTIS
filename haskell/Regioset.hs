70d5de41b50aa509aa93a66e33c4f2ba8e35ab8d
pawel <siegfriedek@gmail.com>
2015-05-18 11:59:41 +0200

    county neighbors unemployed rate

--- haskell/Regioset.hs
+++ haskell/Regioset.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 import Utils
 import Data.Char
-import Data.List.Utils
+import qualified Data.List.Utils as DLU
 import Data.Maybe
 import qualified Data.List as DL
 import qualified Data.Set as S
@@ -10,14 +12,19 @@ import Data.Functor
 import qualified Data.SetMap as SM
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.UTF8 as U
-import qualified Data.Map.Lazy as M
+import qualified Data.Map.Strict as M
 import qualified Data.String.Utils as UT
+import qualified Data.Csv as Csv
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Vector as V
+import qualified Data.ByteString.Lazy.Char8 as BLC
+import Control.Monad
+import Control.Applicative
 
 
-
-data Councillor = Councillor {fullName :: String, year :: Int} deriving (Show)
-data County = County {countyName :: String, neighbors :: [String], councillors :: [Councillor] } deriving (Show)
-data Province = Province {provinceName :: String, counties:: [County]} deriving (Show)
+data Councillor = Councillor {fullName :: String, year :: Int} deriving (Show, Read)
+data County = County {countyName :: String, neighbors :: [String], councillors :: [Councillor] } deriving (Show, Read)
+data Province = Province {provinceName :: String, counties:: [County]} deriving (Show, Read)
 
 class PythonPrint a where
     pythonPrint :: a -> String
@@ -29,10 +36,10 @@ instance PythonPrint County where
 instance PythonPrint Province where
     pythonPrint p = "{ \"provinceName\" : " ++ printPythonString (provinceName p) ++ ",\n \"counties\" : " ++ pythonPrint (counties p) ++ " }"
 instance PythonPrint a => PythonPrint [a] where
-    pythonPrint l = "[ " ++ join ",\n" (map pythonPrint l) ++ " ]"
+    pythonPrint l = "[ " ++ DLU.join ",\n" (map pythonPrint l) ++ " ]"
 
 printStringList :: [String] -> String
-printStringList st = "[ " ++ join ", " (map printPythonString st)  ++ "]"
+printStringList st = "[ " ++ DLU.join ", " (map printPythonString st)  ++ "]"
 printPythonString :: String -> String
 printPythonString s = "\"" ++ s ++ "\""
 
@@ -47,9 +54,55 @@ graphFileName = "graph.txt"
 provincesStartText = "po wyborach w listopadzie 2014 r."
 provincesEndText = "Olgierd GEBLEWICZ"
 encoding = "iso88592"
+unemployedCsvFilePath = "bezrobotni_stopa_wg_powiatow_10_2014.csv"
 
 main :: IO ()
-main = crawlNeighbors
+main = writeUnemployedRateStats
+
+data UnemployedRate = UnemployedRate { provinceCode :: !String, countyCode:: !String, name:: !String, unemployed :: !Double, unemployedRate :: !Double } deriving (Show)
+instance Csv.FromRecord UnemployedRate where
+    parseRecord v
+        | V.length v == 5 = UnemployedRate <$>
+                          v Csv..! 0 <*>
+                          v Csv..! 1 <*>
+                          v Csv..! 2 <*>
+                          v Csv..! 3 <*>
+                          v Csv..! 4
+        | otherwise     = mzero
+
+
+readUnemployedRate :: IO (M.Map String Double)
+readUnemployedRate = do
+    csvData <- BL.readFile unemployedCsvFilePath
+    case Csv.decode Csv.NoHeader csvData of
+        Left err -> return M.empty
+        Right v -> let filtered = V.filter ( (/= "00") . countyCode) v
+                       unemployedRateMap = V.foldl (\acc r -> M.insert (name r) (unemployedRate r) acc) M.empty filtered
+                    in
+                    return unemployedRateMap
+
+data UnemployedRateStats = UnemployedRateStats {regionName :: !String, rate :: !Double, neighborsRate :: !Double, diff :: !Double} deriving (Show)
+instance Csv.ToRecord UnemployedRateStats where
+    toRecord (UnemployedRateStats regionName rate neighborsRate diff) = Csv.record [Csv.toField regionName, Csv.toField rate, Csv.toField neighborsRate, Csv.toField diff]
+
+writeUnemployedRateStats :: IO ()
+writeUnemployedRateStats = do
+    rateMapping <- readUnemployedRate
+    provinces <- load "regiosetProvincesCrawled.txt" :: IO [Province]
+    let allCounties = concatMap counties provinces
+    let stats = map (calculateCountyStats rateMapping) allCounties
+    BLC.putStrLn $ Csv.encode stats
+
+calculateCountyStats :: M.Map String Double -> County -> UnemployedRateStats
+calculateCountyStats rateMapping county = UnemployedRateStats { regionName = countyName county, rate = r,
+                                                                neighborsRate = nrnormalised, diff = r - nrnormalised} where
+                                                                r = getOr0 (countyName county) rateMapping
+                                                                nr = foldl (\acc nname -> acc + getOr0 nname rateMapping) 0 $ neighbors county
+                                                                foundedNeighbors = length $ mapMaybe (`M.lookup` rateMapping) $ neighbors county
+                                                                nrnormalised = nr / fromIntegral foundedNeighbors
+
+getOr0 key map = fromMaybe 0 $ M.lookup key map
+
 
 crawlNeighbors :: IO ()
 crawlNeighbors = do
@@ -58,6 +111,7 @@ crawlNeighbors = do
     provincesPages <- parseRegiosetPages provinceLinks
     provinces <- mapM processProvincePage provincesPages
     putStrLn $ pythonPrint provinces
+    save provinces "regiosetProvincesCrawled.txt"
 
 -- returns suffixes of provinces urls
 getProvinceLinks :: [Tag String] -> [String]
@@ -67,16 +121,16 @@ processProvincePage :: [Tag String] -> IO Province
 processProvincePage tags = do
     let countiesLinks = getCountiesLinks tags
     countiesPages <- parseRegiosetPages countiesLinks
-    let countiesList = filter (not . startswith "Miasto" . countyName) $ map processCountyPage countiesPages
+    let countiesList = filter (not . DLU.startswith "Miasto" . countyName) $ map processCountyPage countiesPages
     return Province {provinceName = pn, counties = countiesList} where
-        pn = UT.strip $ replace "Województwo " "" $ getHeaderText tags
+        pn = UT.strip $ DLU.replace "Województwo " "" $ getHeaderText tags
 
 getCountiesLinks :: [Tag String] -> [String]
 getCountiesLinks tags = mapMaybe extractUrl $ getFragment (containsText "Powiaty w województwie") (containsText "Sąsiednie województwa") tags
 
 processCountyPage :: [Tag String] -> County
 processCountyPage tags = County {countyName = cn, neighbors = ns, councillors = []} where
-                            cn = replace "Powiat " "" $ getHeaderText tags
+                            cn = DLU.replace "Powiat " "" $ getHeaderText tags
                             fragment = getFragment (containsText "Sąsiednie powiaty") (containsText "Samorządowe Kolegium") tags
                             ns = filter (not . DL.isInfixOf "sąsiaduje") $ filter (isAlpha . head) $ map UT.strip $ mapMaybe maybeTagText fragment
 
