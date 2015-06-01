b26c0654be4a365c69e79eda45c0795afb5d2417
Paweł Sikora <siegfriedek@gmail.com>
2015-06-01 12:57:03 +0200

    Crawling councillors

--- haskell/src/Regioset.hs
+++ haskell/src/Regioset.hs
@@ -25,6 +25,7 @@ import qualified System.Exit as SE
 import qualified Graphics.Gnuplot.Frame.OptionSet as Opts
 import qualified Graphics.Gnuplot.Frame.Option as Opt
 import qualified Graphics.Gnuplot.Frame.OptionSet.Histogram as HistOpts
+import qualified Text.Regex.TDFA as R
 
 class PythonPrint a where
     pythonPrint :: a -> String
@@ -51,7 +52,7 @@ instance Csv.FromRecord UnemployedRate where
 instance PythonPrint Councillor where
     pythonPrint c = "( " ++ printPythonString (fullName c) ++ ", " ++ show (year c) ++ " )"
 instance PythonPrint County where
-    pythonPrint c = "( " ++ printPythonString (countyName c) ++ ", " ++ printStringList (neighbors c) ++ " )"
+    pythonPrint c = "( " ++ printPythonString (countyName c) ++ ", " ++ printStringList (neighbors c) ++ ", " ++ pythonPrint (councillors c) ++ " )"
 instance PythonPrint Province where
     pythonPrint p = "{ \"provinceName\" : " ++ printPythonString (provinceName p) ++ ",\n \"counties\" : " ++ pythonPrint (counties p) ++ " }"
 instance PythonPrint a => PythonPrint [a] where
@@ -75,11 +76,13 @@ provincesStartText = "po wyborach w listopadzie 2014 r."
 provincesEndText = "Olgierd GEBLEWICZ"
 encoding = "iso88592"
 unemployedCsvFilePath = "bezrobotni_stopa_wg_powiatow_10_2014.csv"
+radniPowiatuUrlSuffix = "&dr=rady"
 
-main :: IO SE.ExitCode
-main = writeUnemployedRateStats
+-- main :: IO SE.ExitCode
+-- main = writeUnemployedRateStats
 
-
+main :: IO ()
+main = crawlAllProvinces
 
 readUnemployedRate :: IO (M.Map String Double)
 readUnemployedRate = do
@@ -133,8 +136,10 @@ crawlAllProvinces = do
     let provinceLinks = extractProvincesLInks tags
     provincesPages <- parseRegiosetPages provinceLinks
     provinces <- mapM readProvinceFromPage provincesPages
-    putStrLn $ pythonPrint provinces
-    save provinces "regiosetProvincesCrawled.txt" where
+--     putStrLn $ pythonPrint provinces
+    save provinces "regiosetProvincesCrawled.txt"
+    writeFile "withCouncillors.py" (pythonPrint provinces)
+    where
         -- returns suffixes of provinces urls
         extractProvincesLInks tags = mapMaybe extractUrl $ getFragment (containsText provincesStartText) (containsText provincesEndText) tags
 
@@ -142,13 +147,21 @@ readProvinceFromPage :: [Tag String] -> IO Province
 readProvinceFromPage tags = do
     let countiesLinks = extractCountiesLinks tags
     countiesPages <- parseRegiosetPages countiesLinks
-    let countiesList = filter (not . DLU.startswith "Miasto" . countyName) $ map readCountyFromPage countiesPages
-    return Province {provinceName = pn, counties = countiesList} where
+    let countiesPagesWithLinks = zip countiesLinks countiesPages
+    countiesList <- mapM readCountyFromPage countiesPagesWithLinks
+    let noCities = filter (not . DLU.startswith "Miasto" . countyName) countiesList
+    return Province {provinceName = pn, counties = noCities} where
         pn = UT.strip $ DLU.replace "Województwo " "" $ extractHeader tags
         extractCountiesLinks tags = mapMaybe extractUrl $ getFragment (containsText "Powiaty w województwie") (containsText "Sąsiednie województwa") tags
 
-readCountyFromPage :: [Tag String] -> County
-readCountyFromPage tags = County {countyName = cn, neighbors = ns, councillors = []} where
+readCountyFromPage :: (String, [Tag String]) -> IO County
+readCountyFromPage (link, tags) = do
+    [councillorsPage] <- parseRegiosetPages [link ++ radniPowiatuUrlSuffix]
+    let councillorsText =  innerText $ getFragment (containsText "Radni") (~== "</div>") councillorsPage
+        parsedCouncillors = councillorsText R.=~ "\r\n(.*) [(]ur[.] ([0-9]{4})" :: [[String]]
+        councillorsList = map (\(_:name:birthYear:_) -> Councillor {fullName = name, year = read birthYear :: Int} ) parsedCouncillors
+    print $ head councillorsList
+    return County {countyName = cn, neighbors = ns, councillors = councillorsList} where
                             cn = DLU.replace "Powiat " "" $ extractHeader tags
                             fragment = getFragment (containsText "Sąsiednie powiaty") (containsText "Samorządowe Kolegium") tags
                             ns = filter (not . DL.isInfixOf "sąsiaduje") $ filter (isAlpha . head) $ map UT.strip $ mapMaybe maybeTagText fragment
